5359fd2def369a975a0937bbc3028da9d0666747
Kevin Boxhoorn <kevinboxhoorn@yahoo.com>
2016-10-22 10:10:14 +1100

    Add `yaml-mode`

--- init.el
+++ init.el
@@ -50,6 +50,9 @@
   (setq web-mode-css-indent-offset 4)
   (setq web-mode-code-indent-offset 4))
 
+(use-package yaml-mode
+  :mode ("\\.yml\\'" . yaml-mode))
+
 (use-package auto-complete
   :commands (ac-config-default)
   :config (ac-config-default))
